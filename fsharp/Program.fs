1d443ac1dec066cb5ce2935f5aa1e7bad63e97fb
Heidi Mork <heidimork@yahoo.no>
2023-05-14 23:48:15 +0200

    kap 2

---
+++ fsharp/Program.fs
@@ -0,0 +1,29 @@
+﻿open Chapter2.Stacks
+open Chapter2.Trees
+// For more information see https://aka.ms/fsharp-console-apps
+
+let x = Cons(1, (Cons (2, (Cons (3, CustomStack.empty)))))
+let y = Cons(4, (Cons (5, (Cons (6, CustomStack.empty)))))
+
+
+printfn "%A" (CustomStack.add x y)
+
+let z = CustomStack.update x 2 7 
+printfn "%A" z
+
+let lst = [1; 2; 3; 4]
+printfn "%A" <| Oppg_2_1.suffix lst
+
+let subtree1 = T (T (E, 'a', E) ,'b', T(E, 'c', E))
+let subtree2 = T (T (E, 'f', E) ,'g', T(E, 'h', E))
+
+let tree : char Tree = T (subtree1, 'd', subtree2)
+
+printfn "%A" (Tree.memb 'r' tree)
+printfn "%A" (Tree.memb 'g' tree)
+
+let t2 = Tree.insert 'e' tree
+printfn "%A" t2
+
+let dtree = Tree.oppg_2_5_a 'x' 3 
+printfn "%A" dtree
---
+++ fsharp/chapter2/stacks.fs
@@ -0,0 +1,39 @@
+module Chapter2.Stacks
+
+    type 'a Stack = Stack of 'a list
+    
+    module Stack =
+        let empty = Stack []
+        let isEmpty (Stack l) = List.isEmpty l
+        let cons (x: 'a) (Stack s) = Stack (x :: s)
+        let head (Stack l) = List.head l
+        let tail (Stack l) = List.tail l |> Stack
+
+    type 'a CustomStack = Nil | Cons of 'a * 'a CustomStack 
+    module CustomStack = 
+        let empty  = Nil
+        let isEmpty = function Nil -> true | _ -> false
+        let cons x s = Cons (x, s)
+        let head = function 
+            | Nil -> failwith "Emtpy" 
+            | Cons (x, s) -> x
+        let tail = function
+            | Nil -> failwith "Empty"
+            | Cons (x, s) -> s
+        
+        let rec add s1 s2 = 
+            match s1 with
+            | Nil -> s2
+            | Cons (x, xs) -> Cons (x, add xs s2)
+        
+        let rec update (stack: 'a CustomStack) i y = 
+            match stack, i with
+            | Nil, _ -> failwith "Error"
+            | Cons (x, xs), 0 -> Cons(y, xs)
+            | Cons (x, xs), i -> Cons(x, update xs (i - 1) y) 
+        
+    module Oppg_2_1 = 
+        let rec suffix (lst: 'a list) = 
+            match lst with
+            | [] -> [[]]
+            | x :: xs -> lst :: suffix xs
