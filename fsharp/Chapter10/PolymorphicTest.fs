82f7d57aef4339cf92f562dccb4887708d1df8d0
Heidi Mork <heidimork@yahoo.no>
2024-02-26 22:10:12 +0100

    start chap 10

---
+++ fsharp/Chapter10/PolymorphicTest.fs
@@ -0,0 +1,18 @@
+﻿module Chapter10.PolymorphicTest
+
+type 'a Seq = Nil | Cons of 'a * ('a * 'a) Seq
+
+(*
+let rec size (s: 'a Seq): int =
+    match s with
+    | Nil -> 0
+    | Cons (a, ps) -> 1 + 2 * size ps
+*)
+
+type 'a EP = Elem of 'a | Pair of 'a EP * 'a EP
+type 'a Seq' = Nil' | Cons' of 'a EP * 'a Seq'
+
+let rec size (s: 'a Seq'): int =
+    match s with
+    | Nil' -> 0
+    | Cons' (x, ps) -> 1 + 2 * size ps
