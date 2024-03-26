d50d2ec87993dcfdc07ba1d1f7919d5bf4cf97be
Heidi Mork <heidimork@yahoo.no>
2024-03-27 00:01:24 +0100

    10.2

---
+++ fsharp/Chapter10/CatenableList.fs
@@ -0,0 +1,36 @@
+module Chapter10.CatenableList
+
+open FSharpx.Collections
+
+module LazyList = 
+    let snoc a q =
+        LazyList.append q (LazyList.cons a LazyList.empty)
+
+type 'a Cat = E | C of 'a * ('a Cat LazyList)
+
+let head = function
+    | E -> failwith "Illegal operation"
+    | C (x, _) -> x
+
+let link c (ys: 'a Cat) = 
+    match c with
+    | C (x, q) -> C(x, LazyList.snoc ys q)
+
+let (++) x y =
+    match (x, y) with
+    | (E, ys) -> ys
+    | (xs, E) -> xs
+    | (xs, ys) -> link xs ys
+
+let cons x xs = C(x, LazyList.empty) ++ xs
+let snoc xs x = xs ++ C(x, LazyList.empty)
+
+let rec linkAll q = 
+    let t = LazyList.head q
+    let q' = LazyList.tail q
+    if LazyList.isEmpty q' then t else link t (linkAll q')
+
+let tail c = 
+    match c with
+    | E -> failwith "Illegal operation"
+    | C(x, q) -> if LazyList.isEmpty q then E else linkAll q
--- fsharp/Chapter10/Program.fs
+++ fsharp/Chapter10/Program.fs
@@ -1,2 +1,11 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Chapter10.CatenableList
+open FSharpx.Collections
+
+let xl = cons 'a' (cons 'b' (cons 'c' Chapter10.CatenableList.E))
+
+let q = LazyList.cons (C ('a', LazyList.empty)) (LazyList.cons (C ('b', LazyList.empty)) (LazyList.cons (C('c', LazyList.empty)) (LazyList.cons (C('d', LazyList.empty)) LazyList.empty)))
+let xc = C('x', q)
+let t = tail xc
+
+printfn "%A" xc
+printfn "%A" t
